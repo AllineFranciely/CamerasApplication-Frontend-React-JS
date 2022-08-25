9f63f64fa235cfb5066bc28a6d64899cb00baded
Alline <alline_franciely@hotmail.com>
2022-08-25 15:48:42 -0300

    inicia testes

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render,  screen } from '@testing-library/react';
+import Formulario from './components/formulario';
+import Home from './pages/Home';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('1. Testa o componente Formulario', () => {
+  it('Testa se a página Home contém os campos de input', () => {
+    render(<Formulario />);
+
+    const inputName = screen.getByTestId('Nome');
+    expect(inputName).toBeInTheDocument();
+
+    const inputFabricante = screen.getByTestId('Fabricante');
+    expect(inputFabricante).toBeInTheDocument();
+
+    const inputSerie = screen.getByTestId('Serie');
+    expect(inputSerie).toBeInTheDocument();
+  });
+});
+
+describe('2. Testa a página home', () => {
+  it('Testa se a página Home contém o texto correto', () => {
+    render(<Home />);
+
+    const parag1 = screen.getByText(/Lorem ipsum dolor sit amet/i);
+    expect(parag1).toBeInTheDocument();
+  });
 });
