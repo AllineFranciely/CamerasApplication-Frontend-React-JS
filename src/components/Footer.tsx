b9a594f719b0fdc1a40e3d1813fbf1be71b06286
Alline <alline_franciely@hotmail.com>
2022-08-25 12:07:28 -0300

    ajusta footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,19 @@ import './styles/Footer.css';
 function Footer() {
   return (
     <div className="footerInteiro">
-      <p>Alline Franciely</p>
-      <p>2022</p>
+      <button className="buttonOctos" type="button">
+        <a
+          href="https://octos.ai/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Conheça mais
+        </a>
+      </button>
+      <div className="ass">
+        <p>Alline Franciely ©</p>
+        <p>Desafio Octos 2022</p>
+      </div>
     </div>
   );
 }
